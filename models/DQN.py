3c280d40f2ccfdba0d9256c4d45f1d6f0388e00c
andreifurdui <andreifurdui.g@gmail.com>
2019-10-17 13:18:56 +0200

    added policy viz to all

--- models/DQN.py
+++ models/DQN.py
@@ -12,7 +12,7 @@ import gym
 
 class DQN:
     def __init__(self, env_name, replay_memory=BasicReplayMemory):
-        self.num_episodes = 100
+        self.num_episodes = 3000
         self.batch_size = 64
         self.discount_factor = 0.99
         self.learn_rate = 1e-3
@@ -34,10 +34,11 @@ class DQN:
 
     @staticmethod
     def run(env_name):
-        episode_durations, max_positions, max_positions_per_ep = DQN(env_name).__run_episodes()
+        model, episode_durations, max_positions, max_positions_per_ep = DQN(env_name).__run_episodes()
 
         plot.episode_durations(episode_durations)
         plot.episode_durations(max_positions, max_positions_per_ep)
+        plot.visualize_policy(model)
         
     def __train(self):
         if len(self.memory) < self.batch_size:
@@ -110,7 +111,7 @@ class DQN:
             max_positions_per_ep.append(ep_max_position)
 
         print(global_steps)
-        return episode_durations, max_positions, max_positions_per_ep
+        return self.model, episode_durations, max_positions, max_positions_per_ep
 
 
 def compute_q_val(model, state, action):
